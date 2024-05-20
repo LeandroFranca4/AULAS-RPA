c05fa0cb55ea4d7474b4c49f2fe2dd9e2fc32efe
Leandro Franca <leandro24798@gmail.com>
2024-05-20 17:25:09 -0300

    save

---
+++ exe1.py
@@ -0,0 +1,15 @@
+import pyautogui 
+
+pyautogui.moveTo(450,750, duration=2)
+pyautogui.click()
+pyautogui.moveTo(102,227, duration=2)
+pyautogui.click()
+pyautogui.moveTo(331,214,duration=2)
+pyautogui.doubleClick()
+pyautogui.moveTo(359,303, duration=1)
+pyautogui.rightClick()
+pyautogui.moveTo(421,610, duration=1)
+pyautogui.click()
+pyautogui.moveTo(730,600, duration=1)
+pyautogui.moveTo(648,403, duration=1)
+pyautogui.click
