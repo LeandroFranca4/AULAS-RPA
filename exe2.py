943bd5c47b26ea91d85d27d12c09fd4e886fc489
Leandro Franca <leandro24798@gmail.com>
2024-05-22 16:56:42 -0300

    save

--- exe2.py
+++ exe2.py
@@ -1,6 +1,8 @@
 import pyautogui
 
 for c in range (6):
-    pyautogui.moveTo(1713,265,duration=0.2)
+    pyautogui.moveTo(1713,265,duration=1)
 
-    pyautogui.dragTo(2391,311,button='left',duration=0.2)
+    pyautogui.dragTo(2391,311,button='left',duration=1)
+
+    
